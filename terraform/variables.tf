58da41ee6a263eb924d69df240109ccd8d23ba2b
talhasoomro <146392143+talhasoomro@users.noreply.github.com>
2026-01-28 16:08:07 +0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,23 @@
+variable "sql_password" {
+  description = "SQL Admin Password (from GitHub Secrets)"
+  type        = string
+  sensitive   = true
+}
 
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "prod"
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
+}
+
+variable "dr_location" {
+  description = "Disaster recovery region"
+  type        = string
+  default     = "West US"
+}
