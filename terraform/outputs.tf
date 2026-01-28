ac6c15531dfb3f807258190a650792c0c37cca9a
talhasoomro <146392143+talhasoomro@users.noreply.github.com>
2026-01-28 16:14:38 +0500

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,9 @@
+output "webapp_name" {
+  description = "Name of the deployed Azure Web App"
+  value       = azurerm_linux_web_app.app.name
+}
 
+output "resource_group_name" {
+  description = "Resource group containing all resources"
+  value       = azurerm_resource_group.rg.name
+}
