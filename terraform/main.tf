e17ece80f728d723ef6b56915140b281488859fb
talhasoomro <146392143+talhasoomro@users.noreply.github.com>
2026-01-28 16:09:30 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.75"
+    }
+  }
+  
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "tfstatetalha2024"  
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+}
 
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-webapp-${var.environment}"
+  location = var.location
+  
+  tags = {
+    Environment = var.environment
+    Project     = "CloudAssessment"
+    Owner       = "Talha"
+    ManagedBy   = "Terraform"
+  }
+}
