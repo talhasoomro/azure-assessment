438325897f8c8f64efcc23cd2cb311c1346e8660
talhasoomro <146392143+talhasoomro@users.noreply.github.com>
2026-01-28 16:13:42 +0500

    Update storage.tf

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,1 +1,8 @@
-
+resource "azurerm_storage_account" "backup" {
+  name                     = "stbackup${var.environment}talha2024" 
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  min_tls_version          = "TLS1_2"
+}
